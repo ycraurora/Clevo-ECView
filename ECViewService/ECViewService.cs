9691f12f206adf9ed1f2afc7555e8637a11ac43f
YcraD <just4steven@163.com>
2014-12-02 10:27:09 +0800

    ECView Service Added

---
+++ ECViewService/ECViewService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ECViewService
+{
+    public partial class ECViewService : ServiceBase
+    {
+        public ECViewService()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 服务启动动作
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+        }
+        /// <summary>
+        /// 服务关闭动作
+        /// </summary>
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ ECViewService/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ECViewService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ECViewServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.ECViewServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // ECViewServiceProcessInstaller
+            // 
+            this.ECViewServiceProcessInstaller.Password = null;
+            this.ECViewServiceProcessInstaller.Username = null;
+            this.ECViewServiceProcessInstaller.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.serviceProcessInstaller1_AfterInstall);
+            // 
+            // ECViewServiceInstaller
+            // 
+            this.ECViewServiceInstaller.Description = "ECView Service";
+            this.ECViewServiceInstaller.DisplayName = "ECViewService";
+            this.ECViewServiceInstaller.ServiceName = "ECViewService";
+            this.ECViewServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.ECViewServiceProcessInstaller,
+            this.ECViewServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller ECViewServiceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller ECViewServiceInstaller;
+    }
+}
